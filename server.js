efaf33e52786ccf977fcd3f41f574d1b96fe60c4
Shiv Toolsidass <shivtoolsidass@gmail.com>
2016-01-11 16:33:22 -0500

    added package.json

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+
+app.get('/scrape', function(req, res){
+
+});
+
+app.listen('8081');
+console.log("To get some sweet dhall listings, go to port 8081");
+
+exports = module.exports = app;
